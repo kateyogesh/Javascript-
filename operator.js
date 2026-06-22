e406ffcbda901765bd79ab523882bdaf1a34b16d
Yogesh7799. <kateyogesh7799@gmail.com>
2026-06-22 10:43:43 +0530

    new changes

--- operator.js
+++ operator.js
@@ -1,35 +1,3 @@
-let a=10
+let a=30
 
-a++;
-
-console.log(a++)
-
-let b=20
-//b--;
-
-//console.log(++b)
-
-let c=50
-
-let d=60
-
-console.log(c=d)
-
-console.log(c===d)
-//e=f=g=h=20
-//console.log(e,f,g,h)
-
-e=10
-
-console.log("e is",e*=5)
-
-m=true
-console.log(typeof m)
-
-let f=50
-let g=50
-console.log(f!=g)
-console.log(f>g)
-console.log(f<g)
-console.log(f>=g)
-console.log(f<=g)
+console.log(a)
