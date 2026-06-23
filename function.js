2295a0f212e62d938d67ad7f6508c26625ecfa11
Yogesh7799. <kateyogesh7799@gmail.com>
2026-06-23 08:11:47 +0530

    new change hai lala

---
+++ function.js
@@ -0,0 +1,37 @@
+
+function bdd(a){
+
+console.log("Hello word")
+
+
+}
+
+bdd(3)
+
+//Arrow function
+
+const add=()=>{
+    const b=30
+
+    console.log(b)
+}
+
+add()
+
+//Parameter
+
+const yo=(Value)=>{
+
+console.log(`Hi ${Value}`)
+}
+
+yo("raju")
+
+
+
+
+const sanju=(b ,c)=>{
+console.log(`Hi ${b} + ${c}`)
+}
+
+sanju("4","5")
